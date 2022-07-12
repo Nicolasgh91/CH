1cb5d3e51214b94c50c0d4985b960a02cec49687
Nicolasgh91 <nicolasgh91@gmail.com>
2022-07-12 15:10:45 -0300

    Datos y var

---
+++ DatosYvariables.js
@@ -0,0 +1,32 @@
+// 1)
+
+let nombre = 'pepe'
+let edad = 25
+let precio = 99.90
+let seriesFavs = ["Dark", "Mr Robot", "Castlevania"]
+let pelisFavs = [
+    {
+    pelicula: "Interestelar",
+    estreno: 2010,
+    protagonistas: ["Soldan", "Matthew Mcconagey"] },
+    {
+    pelicula: "La pistola desnuda",
+    estreno: 2020,
+    protagonistas: ["Silvio Suller", "Michael Bubble"]
+    }
+]
+// 2)
+console.log(nombre);
+console.log(edad);
+console.log("$" + precio);
+console.log(seriesFavs);
+console.log(pelisFavs);
+// 3)
+edad++
+console.log(edad);
+// 4) 
+seriesFavs.push("Breaking Bad")
+console.log(seriesFavs);
+
+
+
